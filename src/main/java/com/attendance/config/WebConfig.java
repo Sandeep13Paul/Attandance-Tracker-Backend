a261d7fa497ae3aff3a14e3b06a7d380d26390f2
Sandeep <sandeep@MacBook-Pro.local>
2026-04-09 12:44:01 +0530

    updated cors

--- src/main/java/com/attendance/config/WebConfig.java
+++ src/main/java/com/attendance/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://attandance-tracker-front-end.vercel.app/")
+                .allowedOrigins("https://attandance-tracker-front-end.vercel.app")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
